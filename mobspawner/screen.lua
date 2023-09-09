35c663cb2e01316068774742d626b13dd12ce991
Fleyrion <github@xiceta.de>
2023-09-09 22:04:43 +0200

    Making the Screen fancy

--- mobspawner/screen.lua
+++ mobspawner/screen.lua
@@ -1,10 +1,10 @@
 os.loadAPI("variables.lua")
-
-for i,j in ipairs(variables.mobnames) do
-    print(j)
-end
-
+local mobs=0
+local px=0
 x,y = term.getSize()
+for mi,mj in pairs(variables.mobnames) do
+    mobs=mobs+1
+end
 
 function draw()
     for dx=1,x do
@@ -13,5 +13,21 @@ function draw()
             term.write("#")
         end
     end
-    window.create(term.current(), 3, 3, x-4, y-4)
+    win1 = window.create(term.current(), 3, 3, x-4, y-4)
+    win1x,win1y = win1.getSize()
+    win1.setCursorPos(win1x/2,1)
+    center=win1y/2-mobs/2
+    for pi,pj in pairs(variables.mobnames) do
+        win1.setCursorPos(8,center+px)
+        if variables.mobcolor[pi]==colors.white then
+            win1.setTextColor(colors.black)
+            win1.setBackgroudColor(colors.white)
+            win1.write(pj)
+            win1.setTextColor(colors.white)
+        else
+            win1.setBackgroudColor(variables.mobcolor[pi])
+            win1.write(pj)
+        end
+        px=px+1
+    end
 end
